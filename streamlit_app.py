3b0206bb2f88c5eae7a49533881e047ee85c310c
Tom Ferrara <datascience@pancakebreakfaststats.com>
2025-03-20 11:48:44 +0000

    Updated dataset URL in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 
 # Set the URL for the dataset and image
-data_url = 'https://pancakebreakfaststats.com/wp-content/uploads/2025/02/data_file_003.xlsx'
+data_url = 'https://pancakebreakfaststats.com/wp-content/uploads/2025/03/data_file_004.xlsx'
 image_url = 'https://pancakebreakfaststats.com/wp-content/uploads/2024/08/017_logo.png'
 
 # Load and clean the data
